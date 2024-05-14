c26fe4caf29d477a9656dd5d33eb8dd9122dac9c
Nikolina Stojčić <stojcicn00@gmail.com>
2024-05-14 11:12:02 +0200

    ic

---
+++ src/main/java/org/unibl/etf/ip/sni_projekat/SniProjekatApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.sni_projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniProjekatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SniProjekatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unibl/etf/ip/sni_projekat/SniProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.ip.sni_projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniProjekatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
